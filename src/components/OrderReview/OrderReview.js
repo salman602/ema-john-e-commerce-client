910285a5bb4a063cac21c2deff02c685b294dbac
salman602 <salmanrahman602@gmail.com>
2021-10-29 11:33:47 +0600

    added order collection and some problem fixing with post request by keys

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import useProducts from '../../hooks/useProducts';
+
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { removeFromDb } from '../../utilities/fakedb';
 import { useHistory } from 'react-router';
 
 const OrderReview = () => {
-    const [products] = useProducts();
-    const [cart, setCart] = useCart(products);
+    
+    const [cart, setCart] = useCart();
     const history = useHistory();
 
     const handleRemove = key => {
