affb2220cc662f145db90d195d7acae893ba1d37
salman602 <salmanrahman602@gmail.com>
2021-11-02 12:04:43 +0600

    secured user email with the help of firebase authentication authorization

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import useAuth from '../../hooks/useAuth';
 
 const Orders = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
+    const history = useHistory();
     const [orders, setOrders] = useState([]);
-    useEffect(()=>{
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setOrders(data))
-    },[]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`, {
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('id_token')}`,
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else if (res.status === 401){
+                    history.push('/login');
+                }
+            })
+            .then(data => setOrders(data))
+    }, []);
     return (
         <div>
             <h3>Total orders {orders.length}</h3>
